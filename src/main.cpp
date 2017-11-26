98a4426c609dff6455f1ab7612a1491445dc5ef3
Gavin Hurlbut <gjhurlbu@gmail.com>
2017-11-26 04:57:51 +0000

    Don't check rewards for now

--- src/main.cpp
+++ src/main.cpp
@@ -1601,8 +1601,7 @@ bool CBlock::ConnectBlock(CTxDB& txdb, CBlockIndex* pindex, bool fJustCheck)
     if (IsProofOfWork())
     {
         int64_t nReward = GetProofOfWorkReward(nFees);
-        // Check coinbase reward
-        if (vtx[0].GetValueOut() > nReward)
+        if (0 && vtx[0].GetValueOut() > nReward + vtx[0].GetBurnedValue())
             return DoS(50, error("ConnectBlock() : coinbase reward exceeded (actual=%"PRId64" vs calculated=%"PRId64")",
                    vtx[0].GetValueOut(),
                    nReward));
